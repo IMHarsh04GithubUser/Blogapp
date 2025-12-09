e56dad0d35335df99fccd7e496579da5631b4153
dhruvmathur2001 <dhruvmathur2001@gmail.com>
2025-12-09 12:22:34 +0530

    add

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+    
       optimizeCss: false,
       enableBabelRuntime: true,
   },
