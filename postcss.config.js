69e2df025b9273e4938d48e198ad9d7514030d98
dhruvmathur2001 <dhruvmathur2001@gmail.com>
2025-12-09 12:05:41 +0530

    new commit

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,8 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+      optimizeCss: false,
+      enableBabelRuntime: true,
   },
 }
